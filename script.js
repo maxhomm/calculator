bf81937820947e74135dd139ffbe5b297ca8f321
Maxim Gorshkov <blandings@yandex.ru>
2021-08-03 07:28:58 +0300

    added lesson02

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 50000;
+let income = 'Доход от фриланса: 20000 рублей';
+let addExpenses = 'Интернет - 500, транспорт - 4000, коммунальные платежи - 6500, кредит - 2000';
+let deposit = false;
+let mission = 2000000;
+let period = 12;
 
-alert('Добро пожаловать!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('Показываю текст в консоли');
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель: заработать ${mission} рублей`);
+
+addExpenses = addExpenses.toLowerCase().split(', ');
+console.log(addExpenses);
+
+budgetDay = 1900;
+console.log(budgetDay);
