865b730e34158580e8396ebc50e305daaa137bf1
Maxim Gorshkov <blandings@yandex.ru>
2021-08-06 02:33:07 +0300

    added final changes, removed unneccessary logs

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
 
 let money = prompt('Каков ваш месячный доход?');
-console.log(money);
 
 let income = 'Доход от фриланса: 20000 рублей';
 
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log(addExpenses);
 
 
 let deposit = confirm('Есть ли у вас депозит в банке?');
-console.log(deposit);
 
 let expenses1 = prompt('Введите обязательную статью расходов');
-console.log(expenses1);
 let amount1 = prompt('Во сколько это обойдется?');
-console.log(amount1);
 
 let expenses2 = prompt('Введите обязательную статью расходов');
-console.log(expenses2);
 let amount2 = prompt('Во сколько это обойдется?');
-console.log(amount2);
 
 let budgetMonth = money - amount1 - amount2;
-console.log(budgetMonth);
+console.log('Бюджет на месяц: ' + budgetMonth);
 
 let mission = 2000000;
 let period = Math.ceil(mission/budgetMonth);
-console.log(period);
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-
-console.log(addExpenses.length);
-
-console.log(`Период равен ${period} месяцев`);
+console.log(`Цель будет достигнута за ${period} месяцев`);
 console.log(`Цель: заработать ${mission} рублей`);
 
 addExpenses = addExpenses.toLowerCase().split(', ');
 console.log(addExpenses);
 
 budgetDay = Math.floor(budgetMonth/30);
-console.log(budgetDay);
+console.log('Бюджет на день: ' + budgetDay);
 
 if (budgetDay >= 1200) {
     console.log('У вас высокий уровень дохода');
